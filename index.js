cceab9ac7937f2e5cef7832a25d0ef0c3cae35b7
Faysal Akbar <faysalrj361@gmail.com>
2021-10-31 01:35:06 +0600

    initial complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 //Middleware
@@ -17,17 +19,62 @@ async function run() {
         await client.connect();
         const database = client.db("hero_traveller");
         const placeCollection = database.collection("places");
+        const bookedCollection = database.collection("booked");
 
         // GET API
         app.get('/places', async(req, res) => {
         const cursor = placeCollection.find({});
         const result = await cursor.toArray();
         res.send(result);
-})
+        });
+
+        //Find a document
+        app.get('/places/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await placeCollection.findOne(query);
+            res.send(result);
+        })
+
+        // POST API
+        app.post('/addBooking', async(req, res) => {
+            const booking = req.body;
+            const result = await bookedCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        //GET my orders via email
+        app.get('/myOrders/:email', async(req, res) => {
+            const email = req.params.email;
+            const cursor = bookedCollection.find({email: email});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        //DELETE API
+        app.delete('/myOrders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookedCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // GET all orders
+        app.get('/manageOrders', async(req, res) => {
+            const orders = bookedCollection.find({});
+            const result = await orders.toArray();
+            res.send(result);
+        });
+
+        //POST an offer
+        app.post('/addOffer', async(req, res) => {
+            const offer = req.body;
+            const result = await placeCollection.insertOne(offer);
+            res.send(result);
+        });
     }
     finally{
         // await client.close();
-
     }
 }
 run().catch(console.dir);
